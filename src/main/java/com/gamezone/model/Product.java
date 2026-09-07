7805b699d4409cecd3eaccc7cefa6b264adfb0fa
leliecermendoza <leliecermendoza@unicesar.edu.co>
2026-09-06 23:36:29 -0500

    feat: add abstract Product class with common attributes

---
+++ src/main/java/com/gamezone/model/Product.java
@@ -0,0 +1,38 @@
+package com.gamezone.model;
+
+public abstract class Product {
+
+    private String id;
+    private String title;
+    private double price;
+    private int stock;
+
+    public Product(String id, String title, double price, int stock) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public abstract String describe();
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
