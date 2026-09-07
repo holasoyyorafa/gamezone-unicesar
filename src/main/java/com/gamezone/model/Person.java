60c4677763df5b19a60fee15d6e5180dedbed3b2
Ivan Andres Rodriguez Rodriguez <iandresrodriguez@unicesar.edu.co>
2026-09-06 23:55:14 -0500

    feat: add abstract Person class with common attributes

---
+++ src/main/java/com/gamezone/model/Person.java
@@ -0,0 +1,14 @@
+package com.gamezone.model;
+
+public abstract class Person {
+
+    private String id;
+    private String name;
+    private String phone;
+
+    public Person(String id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+}
