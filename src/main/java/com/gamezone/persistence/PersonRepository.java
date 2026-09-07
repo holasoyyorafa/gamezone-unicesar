58ae7f797bdf46f174deae9eec032ca6160acb27
Ivan Andres Rodriguez Rodriguez <iandresrodriguez@unicesar.edu.co>
2026-09-07 00:14:38 -0500

    feat: implement PersonRepository load method

--- src/main/java/com/gamezone/persistence/PersonRepository.java
+++ src/main/java/com/gamezone/persistence/PersonRepository.java
@@ -3,13 +3,14 @@ package com.gamezone.persistence;
 import com.gamezone.model.Client;
 import com.gamezone.model.Person;
 import com.gamezone.model.Seller;
-
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-
+import java.util.ArrayList;
+import java.util.List;
 public class PersonRepository {
 
     private final Path filePath;
@@ -65,5 +66,50 @@ public class PersonRepository {
             Files.createDirectories(parentFolder);
         }
     }
+    public List<Person> load() throws IOException {
+    List<Person> people = new ArrayList<>();
+
+    if (!Files.exists(filePath)) {
+        return people;
+    }
+
+    try (BufferedReader reader = Files.newBufferedReader(filePath)) {
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            if (!line.isBlank()) {
+                Person person = convertFromFileLine(line);
+
+                if (person != null) {
+                    people.add(person);
+                }
+            }
+        }
+    }
+
+    return people;
+}
+private Person convertFromFileLine(String line) {
+    String[] data = line.split("\\|", -1);
+
+    if (data.length == 5 && data[0].equals("CLIENT")) {
+        return new Client(
+                data[1],
+                data[2],
+                data[3],
+                data[4]);
+    }
+
+    if (data.length == 6 && data[0].equals("SELLER")) {
+        return new Seller(
+                data[1],
+                data[2],
+                data[3],
+                data[4],
+                data[5]);
+    }
+
+    return null;
+}
 }
 `
